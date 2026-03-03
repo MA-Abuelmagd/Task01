52090da27a67ac0d7470af01c5f9c026108db259
Ayman <mohamed.ayman7319@gmail.com>
2026-03-03 23:58:38 +0200

    Initial commit: project setup

---
+++ src/main/java/__8078/T_08/Mohamed_Ayman/MohamedAymanApplication.java
@@ -0,0 +1,13 @@
+package __8078.T_08.Mohamed_Ayman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohamedAymanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohamedAymanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/__8078/T_08/Mohamed_Ayman/MohamedAymanApplicationTests.java
@@ -0,0 +1,13 @@
+package __8078.T_08.Mohamed_Ayman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohamedAymanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
